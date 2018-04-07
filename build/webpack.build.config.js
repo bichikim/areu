a227aaa0aa6bb902a83ad0c81be828ed10d2ceb7
Bichi Kim <bichi@live.co.kr>
2018-04-07 12:55:29 +0900

    update fix building error 0.0.9

--- build/webpack.build.config.js
+++ build/webpack.build.config.js
@@ -1,7 +1,5 @@
 const WebpackBaseConfig = require('./webpack.base.config')
 const WebpackMerge = require('webpack-merge')
-// const nodeExternals = require('webpack-node-externals');
-// WebpackBaseConfig.output.libraryTarget = 'umd' // module mode
 
 module.exports = WebpackMerge(WebpackBaseConfig, {
   output: {
@@ -10,10 +8,6 @@ module.exports = WebpackMerge(WebpackBaseConfig, {
   },
   mode: 'production',
   devtool: 'source-map',
-  /**
-   * externals see
-   * @link https://www.npmjs.com/package/webpack-node-externals
-   */
   externals: {
     lodash: {
       commonjs: 'lodash',
